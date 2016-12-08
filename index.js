a28f0085d1e1b0fe2e6d645f4633219a129043a3
jordan-storz <jordan.storz.1@gmail.com>
2016-12-08 15:55:01 -0700

    Create api endpoints

--- index.js
+++ index.js
@@ -11,38 +11,49 @@ app.get('/dishes', (req, res) => {
   knex('dish')
     .then((dishes) => {
       res.json(dishes);
-    });
+    })
+    .catch(err => handleErr(err, res));
 });
 
 app.get('/dishes/:id', (req, res) => {
+  console.log(req.params);
   knex('dish')
     .where('id', req.params.id)
     .first()
     .then((dish) => {
+      if (!dish) {
+        return handleErr('No record found.', res);
+      }
       res.json(dish);
-    });
+    })
+    .catch(err => handleErr(err, res));
 });
 
 app.post('/dishes', (req, res) => {
   let dish = req.body;
   knex('dish')
     .insert(dish)
-    .then((result) => {
-      console.log(result);
-      res.json({message: 'record created'});
+    .returning(['id', 'name', 'price', 'rating'])
+    .then((newDish) => {
+      res.json({
+        message: 'record created',
+        dish: newDish[0]
+      });
     })
     .catch(err => handleErr(err, res));
 });
 
-app.put('/dishes', (req, res) => {
-  let dishId = req.body.id;
-  let dishInfo = req.body.info;
+app.put('/dishes/:id', (req, res) => {
+  let dishId = req.params.id;
+  let dishInfo = req.body;
   knex('dish')
     .where('id', dishId)
     .update(dishInfo)
-    .then((result) => {
-      console.log(result);
-      res.json({message: 'Dish info updated!'});
+    .then((dish) => {
+      res.json({
+        message: 'Dish info updated!',
+        dish: dish[0]
+      });
     })
     .catch(err => handleErr(err, res));
 });
@@ -52,17 +63,16 @@ app.delete('/dishes/:id', (req, res) => {
     .where('id', req.params.id)
     .del()
     .then((result) => {
-      console.log(result);
       res.json({message: 'Dish deleted!'})
     })
     .catch(err => handleErr(err, res));
 });
 
-function handleErr(err, res) {
+function handleErr(error, res) {
   res.status(500);
   res.json({
     message: 'Server error',
-    error: err
+    error
   });
 }
 
